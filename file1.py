cafabde00b8756330003eda5c040dce9aa29da0f
Abhijeetasabe <147067673+Abhijeetasabe@users.noreply.github.com>
2023-10-12 11:49:28 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,9 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> name=input("enter your name")
+... doy=int(input("enter your birth year"))
+... cy=int(input("enter the current year"))
+... age=cy-doy
+... print("dear",name,"!!!! you are",age,"years old")
+... 
+... width=float(input("enter
